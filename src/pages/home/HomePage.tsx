35a154bc0ed616d2f271392683b6f9ae52ab846b
Wilson <77452536+wilson-kbs@users.noreply.github.com>
2024-08-30 11:39:42 +0200

    fix: home layout

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,7 @@
 import EmployeeForm, {
   EmployeeFormInput,
 } from "@/components/forms/EmployeeForm/EmployeeForm.tsx";
+import style from "./HomePage.module.css";
 import { addEmployee } from "@/app/slices/employeesSlice";
 import { useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
@@ -19,13 +20,13 @@ function HomePage() {
   };
 
   return (
-    <div>
+    <div className={style.WrapperPage}>
       <div className="title">
         <h1>HRnet</h1>
       </div>
-      <div className="container">
-        <NavLink to={"/employee-list"}>View Current Employees</NavLink>
-        <h2>Create Employee</h2>
+      <div className={style.Container}>
+        <NavLink to={"/employee-list"} className={style.Container_Link}>View Current Employees</NavLink>
+        <h2 className={style.Container_Title}>Create Employee</h2>
         <EmployeeForm onSubmit={handleSubmit} />
       </div>
     </div>
